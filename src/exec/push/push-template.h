fbcbd037b9ab54b8c9d5a68ceebe02bb24cf800c
jyy <njujiangyy@gmail.com>
2014-10-25 17:51:18 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  17:51:18 up  6:51,  2 users,  load average: 0.40, 0.14, 0.09 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---tmux      |-tmux-+-bash      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      `-bash---vim      `-udevd---2*[udevd]

--- src/exec/push/push-template.h
+++ src/exec/push/push-template.h
@@ -4,8 +4,9 @@
 
 make_helper(concat(push_r2r_,SUFFIX)){
 	int reg_code = instr_fetch(eip,1) & 0x7;
-	REG(4)=REG(4)-4;
-	MEM_W(REG(4),REG(reg_code));
+	REG(R_ESP)=REG(R_ESP)-DATA_BYTE;
+	MEM_W(REG(R_ESP),REG(reg_code));
+	print_asm("push" "%%%s",REG_NAME(reg_code));
 	return 1;
 }
 
